5c4076e3a9a426531c1187e01f60ea52de54b30d
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2025-02-14 19:39:29 +0500

    second-commit

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,19 @@
-print("Hello World")
+# print("My Name is Hamiz")
+# print("Hello World")
+# print(23)
+
+
+name = "Hamiz"
+age = 19
+percentage = 87.8 
+
+print("My name is", name)
+print(type(name))
+print(type(age))
+print(type(percentage))
+
+#Single Line
+
+'''
+Multi Line Comments 
+'''
