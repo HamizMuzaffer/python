13f37adc59deeb0b74cf92e803549b8fa5fbb005
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2025-02-22 04:33:44 +0500

    practice questions added

--- firstprogram.py
+++ firstprogram.py
@@ -43,20 +43,95 @@ Multi Line Comments
 # movies.append(movie3)
 # print(movies)
 
-list = [1,"abc","abc",1]
+# list = [1,"abc","abc",1]
 
-copyList = list.copy()
-copyList.reverse()
+# copyList = list.copy()
+# copyList.reverse()
 
-if(copyList == list):
-    print("It is Palindrome")
-else: print("It is not")
-
-
-tuple = ("C","D","A","A","B","B","A")
-
-print(tuple.count("A"))
+# if(copyList == list):
+#     print("It is Palindrome")
+# else: print("It is not")
 
 
-## Dictionary and sets 
+# tuple = ("C","D","A","A","B","B","A")
 
+# print(tuple.count("A"))
+
+
+# ## Dictionary and sets 
+# info = {
+#     "name" : "Hamiz Muzaffer",
+#     "subjects": ["DSA","DB","DDA"],
+#     "age" : 20,
+#     "isAdult" : True,
+#     "marks": 24.4
+# }
+
+# # print(info["name2"]) #error
+# # print(info.get("name")) #none
+
+# info.update({"city" : "karachi"})
+# print(info)
+
+
+
+
+#sets 
+
+# set = {1,2,4,5,4}
+# print(set)
+
+# collection = {} # empty dictionary
+
+collection = set() # empty set 
+# collection.add(2)
+# collection.add(3)
+# collection.add(4)
+# collection.add(5)
+# collection.clear()
+# collection.pop()
+# print(len(collection))
+
+
+# set1 = {1,2,3,4}
+# set2 = {4,5,6,7}
+
+# print(set1.intersection(set2))
+
+
+## Question 1 
+
+# dict = {
+#     "table" : ["list of facts and figures","a piece of furniture"],
+#     "cat" : "a small animal"
+# }
+
+# print(dict)
+
+
+## Question 2 
+
+# set = {"python","java","C++","python","javascript","java","python","java","C++","c"}
+
+# print(len(set))
+
+
+## Question 3 
+
+# dict = {}
+
+# dsa = input("Enter Marks")
+# dict.update({"DSA": dsa})
+# db = input("Enter Marks")
+# dict.update({"DB": db})
+# daa = input("Enter Marks")
+# dict.update({"DAA": daa})
+
+# print(dict)
+
+
+## Question 4 
+
+set = {("int",9),("float",9.0)}
+
+print(set)
